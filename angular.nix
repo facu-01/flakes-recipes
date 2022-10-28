12b343d90c1c2e14472a0e4097159c2c2ece95a3
facu-01 <facundon.lucero@alumnos.frm.utn.edu.ar>
2022-10-28 14:03:11 -0300

    angular

---
+++ angular.nix
@@ -0,0 +1,33 @@
+{
+  description = "Development environment Angular - Node";
+
+  inputs = {
+    nixpkgs = { url = "github:NixOS/nixpkgs/nixpkgs-unstable"; };
+    flake-utils = { url = "github:numtide/flake-utils"; };
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        inherit (nixpkgs.lib) optional;
+        pkgs = import nixpkgs { inherit system; };
+
+      in
+
+      {
+        devShell = pkgs.mkShell
+          {
+            buildInputs = with pkgs;[
+              nodejs-16_x
+              nodePackages."@angular/cli"
+            ];
+
+            shellHook = ''
+              echo node environment
+              echo angular version
+              ng v
+            '';
+          };
+      }
+    );
+}
